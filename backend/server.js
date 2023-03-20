0d246a7fbd2f8c9c0a9cd1457dd9ecb546d7ac7d
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-03-20 22:20:41 +0530

    mongoDB connected by mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express=require('express');
+const mongoose=require('mongoose');
 const workoutRoutes=require('./routes/workout');
 
 //express app
@@ -20,7 +21,16 @@ app.use('/api/workouts',workoutRoutes);
 //     res.json({mssg:"welcome to the app"})
 // })
 
-//listens for request
-app.listen(process.env.PORT,()=>{
-    console.log("listening on port",process.env.PORT);
-})
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("Connected to DB");
+        //listens for request
+        app.listen(process.env.PORT,()=>{
+            console.log("listening on port",process.env.PORT);
+        })
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
