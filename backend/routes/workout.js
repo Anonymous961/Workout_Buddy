c0a6cee073a06f0dbb382de1e98d82d96d70718e
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-03-21 22:01:51 +0530

    workout schema and model added

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,5 +1,8 @@
 const express=require('express');
+const Workout = require('../models/workoutModel');
+
 const router=express.Router();
+
 
 //Get all workouts
 router.get("/",(req,res)=>{
@@ -13,6 +16,14 @@ router.get("/:id",(req,res)=>{
 
 //Post a new workout
 router.post("/",(req,res)=>{
+    const {title, reps,load }= req.body;
+
+    try{
+        const workout=Workout.create(title,reps,load);
+        res.status(200).json(workout);
+    }catch(error){
+        res.status(400).json({error: error.message});
+    }
     res.json({mssg:"Post a new workout!"});
 })
 
