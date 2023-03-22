1ce85a5cf92d2754c465df9430352a0fa48f37dd
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-03-22 20:56:31 +0530

    workoutDController updated, workout.js updated

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,4 +1,6 @@
 const Workout = require('../models/workoutModel');
+const mongoose=require("mongoose");
+const { request } = require('express');
 
 //get all workouts
 const getWorkouts= async (req,res)=>{
@@ -8,7 +10,22 @@ const getWorkouts= async (req,res)=>{
 }
 
 //get a single workout
+const getWorkout= async (req,res)=>{
+    const {id}=req.params;
 
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"not a valid ID."});
+    }
+    const workout=await Workout.findById(id);
+
+    if(!workout){
+        return res.status(404).json({error:"no such workout found"});
+    }
+
+    res.status(200).json(workout);
+
+    // res.json({mssg:"Get a single workout!"});
+}
 
 //create a new workout
 const createWorkout = async (req,res)=>{
@@ -24,13 +41,45 @@ const createWorkout = async (req,res)=>{
 }
 
 // delete a workout
+const deleteWorkout= async(req,res)=>{
 
+    const {id}=req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"not a valid ID."});
+    }
+
+    const workout= await Workout.findOneAndDelete({_id:id})
+
+    if(!workout){
+        return res.status(404).json({error:"no such workout found."});
+    }
+
+    res.status(200).json(workout);
+}
 
 // update a workout
+const updateWorkout = async (req,res)=>{
+    const {id}=req.params;
 
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"not a valid ID."});
+    }
+
+    const workout= await Workout.findOneAndUpdate({_id:id},{...req.body})
+
+    if(!workout){
+        return res.status(404).json({error:"no such workout found."});
+    }
+
+    res.status(200).json(workout);
+}
 
 
 module.exports = {
     createWorkout,
-    getWorkouts
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
 }
--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,7 +1,10 @@
 const express=require('express');
 const {
     createWorkout,
-    getWorkouts
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
 }=require('../controllers/workoutController');
 
 const router=express.Router();
@@ -11,22 +14,16 @@ const router=express.Router();
 router.get("/",getWorkouts);
 
 //Get a single workout
-router.get("/:id",(req,res)=>{
-    res.json({mssg:"Get a single workout!"});
-})
+router.get("/:id",getWorkout);
 
 //Post a new workout
 router.post("/",createWorkout);
 
 //Delete a workout
-router.delete("/:id",(req,res)=>{
-    res.json({mssg:"Delete a new workout!"});
-})
+router.delete("/:id",deleteWorkout);
 
 //Update a workout
-router.patch("/:id",(req,res)=>{
-    res.json({mssg:"Update a workout!"});
-})
+router.patch("/:id",updateWorkout);
 
 
 module.exports=router;
