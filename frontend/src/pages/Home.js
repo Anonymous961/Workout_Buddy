7a2f012228625a2f81896cfdba413aaca00bae9b
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-03-23 19:33:17 +0530

    proxy added, fetch added

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,26 @@
+import {useState,useEffect} from 'react';
+
 const Home = () => {
+    const [workouts,setWorkouts]=useState(null);
+
+    useEffect(()=>{
+        const fetchWorkout=async ()=>{
+            const res= await fetch("/api/workouts");
+            const json=await res.json();
+
+            if(res.ok){
+                setWorkouts(json);
+            }
+        }
+        fetchWorkout();
+    },[])
     return ( 
         <div className="Home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout)=>[
+                    <p key={workout._id}>{workout.title}</p>
+                ])}
+            </div>
         </div>
     );
 }
