6040afc589f7deca73da0964441b480a4aa09848
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-05-28 18:56:45 +0530

    static signup model created(Password salting,hashing)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,18 @@
+const User=require("../models/userModel")
 //login
 const loginUser= async (req,res)=>{
     res.json({mssg:'login user'})
 }
 const signupUser= async (req,res)=>{
+    const {email,password}=req.body;
+
+    try{
+        const user= await User.signup(email,password);
+
+        res.status(200).json({email,user});
+    }catch(error){
+        res.status(400).json({error:error.message})
+    }
     res.json({mssg:'signup user'})
 }
 
