90d00c05fdd5c13ebe77a858ffb6bedc53159321
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-05-28 23:16:24 +0530

    email and password validations, fixing stack problems

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,35 +1,47 @@
-const mongoose=require('mongoose')
-const Schema=mongoose.Schema;
-const bcrypt=require("bcrypt");
-const {hash} = require("bcrypt");
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
-const userSchema= new Schema({
-    email:{
-        type:String,
-        required:true,
-        unique:true
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
+    password: {
+        type: String,
         required: true
     }
 })
 
-//static signup method
-userSchema.statics.signup=async function(email,password){
+// static signup method
+userSchema.statics.signup = async function(email, password) {
 
-    const exists=await this.findOne({email});
-
-    if(exists){
-        throw Error("Email already in use!");
+    // validation
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
     }
 
-    const salt= await bcrypt.genSalt(10);
-    const hash= await bcrypt.hash(password,salt);
+    const exists = await this.findOne({ email })
 
-    const user=await this.create({email,password:hash});
+    if (exists) {
+        throw Error('Email already in use')
+    }
 
-    return user;
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash })
+
+    return user
 }
 
-module.exports=mongoose.model("User",userSchema);
+module.exports = mongoose.model('User', userSchema)
