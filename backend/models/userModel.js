e43d6f3a823df184ee723a2480ccbfbe323c0737
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-06-12 17:08:40 +0530

    made static login object and user token in userSchema just to make user login

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const validator = require("validator");
+const { findOne } = require("./workoutModel");
 
 const Schema = mongoose.Schema;
 
@@ -39,6 +40,27 @@ userSchema.statics.signup = async function (email, password) {
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+userSchema.statics.login = async function (email, password) {
+  //validation
+  if (!email || !password) {
+    throw Error("All fields must be filled!");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("not a valid user");
+  }
+  //if the users password matches with the input
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Password is not correct!");
+  }
 
   return user;
 };
