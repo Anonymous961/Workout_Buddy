40249651b1bf7a2285c6cc3718f1d9e35a515fcb
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-06-13 18:25:23 +0530

    middleware of auth added

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,29 +1,30 @@
-const express=require('express');
+const express = require("express");
 const {
-    createWorkout,
-    getWorkouts,
-    getWorkout,
-    deleteWorkout,
-    updateWorkout
-}=require('../controllers/workoutController');
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController");
+const requireAuth = require("../middleware/requireAuth");
 
-const router=express.Router();
+const router = express.Router();
 
-
+//reuire auth for all workout routes
+router.use(requireAuth);
 //Get all workouts
-router.get("/",getWorkouts);
+router.get("/", getWorkouts);
 
 //Get a single workout
-router.get("/:id",getWorkout);
+router.get("/:id", getWorkout);
 
 //Post a new workout
-router.post("/",createWorkout);
+router.post("/", createWorkout);
 
 //Delete a workout
-router.delete("/:id",deleteWorkout);
+router.delete("/:id", deleteWorkout);
 
 //Update a workout
-router.patch("/:id",updateWorkout);
+router.patch("/:id", updateWorkout);
 
-
-module.exports=router;
+module.exports = router;
