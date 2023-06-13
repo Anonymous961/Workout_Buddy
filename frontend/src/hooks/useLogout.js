0b6c6952658a8e7fef9f895b1317d8ec381708b2
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-06-13 20:39:55 +0530

    cleared previous user data

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,13 +1,16 @@
 import { useAuthContext } from "./useAuthContext";
+import { useWorkoutsContext } from "./useWorkoutsContext";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: workoutDispatch } = useWorkoutsContext();
   const logout = () => {
     //remove user from storage
     localStorage.removeItem("user");
 
     //dispatch logout action
     dispatch({ type: "LOGOUT" });
+    workoutDispatch({ type: "SET_WORKOUT", payload: null });
   };
 
   return { logout };
