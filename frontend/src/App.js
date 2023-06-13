8c7097538f4b74d4e96c789604eb5e6ee43464eb
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-06-13 18:55:52 +0530

    navigating if there is a user else login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Login from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
+import { useAuthContext } from "./hooks/useAuthContext";
 function App() {
+  const { user } = useAuthContext();
   return (
     <div className="App">
       <Router>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={user ? <Navigate to="/" /> : <Login />}
+            />
+            <Route
+              path="/signup"
+              element={user ? <Navigate to="/" /> : <SignUp />}
+            />
           </Routes>
         </div>
       </Router>
