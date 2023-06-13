62751ee27c44d4aa76e6adae4a092c117567f309
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-06-13 12:13:31 +0530

    AuthContextProvider added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { WorkoutsContextProvider } from './context/WorkoutsContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { WorkoutsContextProvider } from "./context/WorkoutsContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <WorkoutsContextProvider>
+    <AuthContextProvider>
+      <WorkoutsContextProvider>
         <App />
-    </WorkoutsContextProvider>
+      </WorkoutsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
