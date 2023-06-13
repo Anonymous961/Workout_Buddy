a971df0a8385547d9a12dc88250d00230fdabdb4
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-06-13 18:58:52 +0530

    delete patch

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -14,7 +14,7 @@ const WorkoutDetails = ({ workout }) => {
     const response = await fetch("/api/workouts/" + workout._id, {
       method: "DELETE",
       headers: {
-        Authorization: `${user.token}`,
+        Authorization: `Bearer ${user.token}`,
       },
     });
     const json = await response.json();
