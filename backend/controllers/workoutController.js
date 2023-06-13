f4c85175b8dd62423333a360b3326cd4224fb479
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-06-13 20:37:12 +0530

    separated user data by user id

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,100 +1,103 @@
-const Workout = require('../models/workoutModel');
-const mongoose=require("mongoose");
-const { request } = require('express');
+const Workout = require("../models/workoutModel");
+const mongoose = require("mongoose");
+const { request } = require("express");
 
 //get all workouts
-const getWorkouts= async (req,res)=>{
-    const workouts = await Workout.find({}).sort({createdAt:-1});
+const getWorkouts = async (req, res) => {
+  const user_id = req.user._id;
+  const workouts = await Workout.find({ user_id }).sort({ createdAt: -1 });
 
-    res.status(200).json(workouts);
-}
+  res.status(200).json(workouts);
+};
 
 //get a single workout
-const getWorkout= async (req,res)=>{
-    const {id}=req.params;
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error:"not a valid ID."});
-    }
-    const workout=await Workout.findById(id);
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "not a valid ID." });
+  }
+  const workout = await Workout.findById(id);
 
-    if(!workout){
-        return res.status(404).json({error:"no such workout found"});
-    }
+  if (!workout) {
+    return res.status(404).json({ error: "no such workout found" });
+  }
 
-    res.status(200).json(workout);
+  res.status(200).json(workout);
 
-    // res.json({mssg:"Get a single workout!"});
-}
+  // res.json({mssg:"Get a single workout!"});
+};
 
 //create a new workout
-const createWorkout = async (req,res)=>{
-    const {title, load, reps }= req.body;
+const createWorkout = async (req, res) => {
+  const { title, load, reps } = req.body;
 
-    let emptyFields = [];
+  let emptyFields = [];
 
-    if(!title){
-        emptyFields.push('title');
-    }
-    if(!load){
-        emptyFields.push('loads');
-    }
-    if(!reps){
-        emptyFields.push('reps');
-    }
-    if(emptyFields.length>0){
-        return res.status(400).json({error:"Please fill in all the fields.",emptyFields})
-    }
-    
-    // add doc to db
-    try{
-        const workout=await Workout.create({title,load,reps});
-        res.status(200).json(workout);
-    }catch(error){
-        res.status(400).json({error: error.message});
-    }
-}
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (!load) {
+    emptyFields.push("loads");
+  }
+  if (!reps) {
+    emptyFields.push("reps");
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all the fields.", emptyFields });
+  }
+
+  // add doc to db
+  try {
+    const user_id = req.user._id;
+    console.log(user_id);
+    const workout = await Workout.create({ title, load, reps, user_id });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 // delete a workout
-const deleteWorkout= async(req,res)=>{
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
 
-    const {id}=req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "not a valid ID." });
+  }
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error:"not a valid ID."});
-    }
+  const workout = await Workout.findOneAndDelete({ _id: id });
 
-    const workout= await Workout.findOneAndDelete({_id:id})
+  if (!workout) {
+    return res.status(404).json({ error: "no such workout found." });
+  }
 
-    if(!workout){
-        return res.status(404).json({error:"no such workout found."});
-    }
-
-    res.status(200).json(workout);
-}
+  res.status(200).json(workout);
+};
 
 // update a workout
-const updateWorkout = async (req,res)=>{
-    const {id}=req.params;
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error:"not a valid ID."});
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "not a valid ID." });
+  }
 
-    const workout= await Workout.findOneAndUpdate({_id:id},{...req.body})
+  const workout = await Workout.findOneAndUpdate({ _id: id }, { ...req.body });
 
-    if(!workout){
-        return res.status(404).json({error:"no such workout found."});
-    }
+  if (!workout) {
+    return res.status(404).json({ error: "no such workout found." });
+  }
 
-    res.status(200).json(workout);
-}
-
+  res.status(200).json(workout);
+};
 
 module.exports = {
-    createWorkout,
-    getWorkouts,
-    getWorkout,
-    deleteWorkout,
-    updateWorkout
-}
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout,
+};
