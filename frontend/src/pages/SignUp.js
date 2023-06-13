7d425f3faffcad6440acabf33993eabc212eabd5
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-06-13 15:04:30 +0530

    signup custom hook

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,13 +1,15 @@
 import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { signup, error, isLoading } = useSignup();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(email, password);
+    await signup(email, password);
   };
   return (
     <form className="signup" onSubmit={handleSubmit}>
@@ -30,7 +32,10 @@ const SignUp = () => {
         placeholder="password"
         required
       />
-      <button type="submit">Sign Up</button>
+      <button type="submit" disabled={isLoading}>
+        Sign Up
+      </button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
