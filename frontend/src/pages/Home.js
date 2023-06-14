a252927021136ddead094fb5e7b8f4b26e0a341c
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-06-14 16:28:53 +0530

    api link added after depolyment

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -10,11 +10,14 @@ const Home = () => {
   const { user } = useAuthContext();
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch("/api/workouts", {
-        headers: {
-          Authorization: `Bearer ${user.token}`,
-        },
-      });
+      const response = await fetch(
+        "https://workout-buddy-tawny.vercel.app/api/workouts",
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
       const json = await response.json();
 
       if (response.ok) {
