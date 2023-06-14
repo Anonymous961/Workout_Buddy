d202ebf6b0a11d79319d8d828b9772e4a776877d
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-06-14 16:34:59 +0530

    Revert "api link added after depolyment"
    This reverts commit a252927021136ddead094fb5e7b8f4b26e0a341c.

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -10,14 +10,11 @@ export const useLogin = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch(
-      "https://workout-buddy-tawny.vercel.app/api/user/login",
-      {
-        method: "POST",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      }
-    );
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
 
     const json = await response.json();
 
