7908124e676fec8cadd5a4f998b05856133c91c2
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-06-23 18:35:02 +0530

    password strong message updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,7 +27,9 @@ userSchema.statics.signup = async function (email, password) {
     throw Error("Email not valid");
   }
   if (!validator.isStrongPassword(password)) {
-    throw Error("Password not strong enough");
+    throw Error(
+      "Password not strong enough.Password must contain capital letter,small letter,number and symbal."
+    );
   }
 
   const exists = await this.findOne({ email });
